3e14fa835086c74ca467697039b972c01fb0223f
Ashwed Palve <ashwedp@gmail.com>
2026-09-07 14:59:15 +0000

    add initial server setup with Express and required dependencies

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const methodoverride = require("method-override");
+const mongoose = require('mongoose');
+const ejs = require("ejs");
+const ejsmate = require("ejs-mate");
